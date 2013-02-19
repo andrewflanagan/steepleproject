75caf3f62b73686eab170a38054c08877997063b
Andrew Flanagan <andrew.flanagan@gmail.com>
2013-02-18 23:38:30 -0800

    An extremely basic Rails app -- the initial framework for the Steeple Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Steepleproject::Application.config.secret_token = 'b79e39d0739f0b7127a6d1cce1ee9f88b2435ef7545ff51dd720d0fff74afb44d65aa73e9de0009c4c58ca627a46490596d506aac4afe33d762ecfc4c28a0b9a'
